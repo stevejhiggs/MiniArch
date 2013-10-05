717995c32a95b1974be4be785781deca39ec4d6a
steve higgs <stevejhiggs@gmail.com>
2013-10-05 11:53:59 +0100

    Add serviceLayer test assembly

--- MiniArch.ServiceLayer.Tests/Properties/AssemblyInfo.cs
+++ MiniArch.ServiceLayer.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MiniArch.ServiceLayer")]
+[assembly: AssemblyTitle("MiniArch.ServiceLayer.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MiniArch.ServiceLayer")]
+[assembly: AssemblyProduct("MiniArch.ServiceLayer.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("08311844-d819-46ba-a9cb-8b746ec855af")]
+[assembly: Guid("ddae5438-683b-40bc-b859-ab24ea07de7f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("MiniArch.ServiceLayer.Tests")]
